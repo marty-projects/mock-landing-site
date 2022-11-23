136f40e858ce0049ed440a11800082ab61b6c7a7
maraty-projects <maupamaupa497@gmail.com>
2022-11-23 14:58:45 +0100

    first commit

---
+++ src/containers/whatGTP3/WhatGTP3.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './whatGTP3.css';
+import { Feature } from '../../components/';
+
+
+const WhatGTP3 = () => {
+  return (
+    <div className="gtp3__ whatgtp3 section__margin" id="whatgtp3">
+        <div className="gtp3__whatgtp3-feature">
+            <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
+        </div>
+        <div className="gtp3__whatgtp3-heading">
+            <h1 className="gradient__text">
+            The possibilities are beyond your imagination
+            </h1>
+            <p>Explore the Library</p>
+            <div className="gtp3__whatgtp3-container">
+                <Feature title="Chatbot" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."/>
+                <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"/>
+                <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WhatGTP3
